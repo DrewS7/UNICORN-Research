d72c8aeecfb525d7424cc89a13f672ef87d3d28a
DrewS7 <132096212+DrewS7@users.noreply.github.com>
2023-10-11 22:13:07 -0400

    GCP needs PNG, not TIFF

---
+++ convTiff2png.py
@@ -0,0 +1,33 @@
+# From ChatGPT
+
+from PIL import Image
+import os
+
+# Specify the input folder containing .tiff images
+input_folder = "C:/Purdue/LeGrand/WIP/datasets/xviewLatest/train/imagesTiff"
+
+# Specify the output folder where you want to save .png images
+output_folder = "C:/Purdue/LeGrand/WIP/datasets/xviewLatest/train/imagesPNG"
+
+# Create the output folder if it doesn't exist
+# os.makedirs(output_folder, exist_ok=True)
+
+# List all files in the input folder
+file_list = os.listdir(input_folder)
+
+# Loop through each file in the input folder
+for file_name in file_list:
+    if file_name.endswith(".tiff") or file_name.endswith(".tif"):
+        # Open the .tiff image
+        image = Image.open(os.path.join(input_folder, file_name))
+
+        # Construct the output file path with the .png extension
+        output_file = os.path.splitext(file_name)[0] + ".png"
+        output_path = os.path.join(output_folder, output_file)
+
+        # Convert and save the image as .png
+        image.save(output_path, "PNG")
+
+        print(f"Converted {file_name} to {output_file}")
+
+print("Conversion complete.")
